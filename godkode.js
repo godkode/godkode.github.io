1622f2c96cc117b05dbfd60bda180f8a23a2f91d
godkode <godkodedk@gmail.com>
2016-08-22 20:45:11 +0200

    Add files via upload
    add from github.com

---
+++ godkode.js
@@ -0,0 +1,171 @@
+var reader; //GLOBAL File Reader object for demo purpose only
+
+/**
+ * Check for the various File API support.
+ */
+function checkFileAPI() {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        reader = new FileReader();
+        return true; 
+    } else {
+        alert('The File APIs are not fully supported by your browser. Fallback required.');
+        return false;
+    }
+
+}
+var divEL; // GLOBAL: So i can write to it any where
+function displayContent() {
+    divEL = document.getElementById('contentID');
+
+    // Get data and rework to data Array
+    readSheet();
+}
+
+var countCell = 0;
+
+// Main HTML builder
+// Parameter single Array with max length of 7
+function htmlReturn(txt) {
+  	var valueReturn = "";
+
+	// This is the Div of the coloum.. :)
+	valueReturn += "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-4 borderMeTop'>";
+
+	var title = "<H4><center>" + txt[2] + "</center></H4>";
+
+	var status = "<ul><li><b>Status: </b>" + txt[1] + "</li>"; 
+	var time = "<li><b>Date: </b>" + txt[4] + "</li>";
+	var room = "<li><b>Room:</b>" + txt[6] + "</li>";
+	var who = "<li><b>Who:</b> " + txt[7] + "</li>"; 
+		
+	var invited = "<li><b>Invited:</b> " + txt[5] + "</li>";
+
+	var description = "<b><li>Description: </b></li><br>" + txt[3];
+
+	valueReturn += 	title + "" +
+					status + "" +
+				   	time + "" +  
+				   	room + "" +  
+				   	who + "" +  
+				   	invited + "" +  
+				   	description;
+	valueReturn += "</ul>";
+	valueReturn += "</div>";
+
+  	// Html buils DONE 
+  	countCell++;
+  	return valueReturn;
+}
+
+// data put in eventDataArray
+// BUILD: HTML from here
+// All data lost after getJSON
+function getValue(event) {
+	if (countCell % 3 == 0) {
+  		divEL.innerHTML += "<div class='row'>";
+  		console.log("Start row");
+  	}
+
+	divEL.innerHTML += htmlReturn(event);
+
+	if (countCell > 2 && countCell % 3 == 2) {
+		// End Row
+		divEL.innerHTML += "</div>";
+		console.log("End row");
+	} else if (countCell == 2) {
+		// End Row
+		divEL.innerHTML += "</div>";
+		console.log("End row");
+	}
+}
+
+// New reader from 
+// SOURCE: https://ctrlq.org/code/20004-google-spreadsheets-json
+function readSheet() {
+	
+    // ID of the Google Spreadsheet
+    var spreadsheetID = "1Nfm0H4nIH_NSBde2mZMgD3ssYMiLsa0T4-dd-GfEQXI";
+ 
+    // Make sure it is public or set to Anyone with link can view 
+    var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+    
+	var dataHolderOfArrays = [];
+    var j = 0;
+
+    $.getJSON(url, function(data) {
+ 
+        var entry = data.feed.entry;
+        //console.log(entry);
+
+        for (var i = 3; i < entry.length; i++) {
+            
+            var dataHolderArray = [];
+            var counterArrat = 0;
+            var date = entry[i].gsx$_cokwr.$t + " " + entry[i].gsx$_cpzh4.$t;
+            dataHolderArray[counterArrat] = date;
+            counterArrat++;
+
+            i++;
+			// i = 4
+
+            // status
+            dataHolderArray[counterArrat] = isData(entry[i].gsx$_cpzh4);
+            counterArrat++;
+
+            // title
+            dataHolderArray[counterArrat] = isData(entry[i].gsx$_chk2m);
+            counterArrat++;
+
+            //var title = entry[i].gsx$_chk2m.$t;
+            
+            // description;
+            dataHolderArray[counterArrat] = isData(entry[i].gsx$_ciyn3);
+            counterArrat++;
+
+            // time
+            dataHolderArray[counterArrat] = isData(entry[i].gsx$_cx0b9);
+            counterArrat++;
+            //var time = entry[i].gsx$_cx0b9.$t;
+            i += 2;
+            // i = 6
+
+            // invite
+            dataHolderArray[counterArrat] = isData(entry[i].gsx$_cpzh4);
+            counterArrat++;
+
+            //var invite = entry[i].gsx$_cpzh4.$t;
+            i++;
+			// i = 7
+            // room
+            dataHolderArray[counterArrat] = isData(entry[i].gsx$_cpzh4);
+            counterArrat++;
+
+            i++;
+            // i = 8
+            // company/who
+            dataHolderArray[counterArrat] = isData(entry[i].gsx$_cpzh4);
+                      
+            
+            //var company = entry[i].gsx$_cpzh4.$t;
+
+            //dataHolder[j] = dataHolderArray;
+            j++;
+            i += 2;
+            // i = 10
+            getValue(dataHolderArray);
+        } // i = 11
+
+        //console.log(dataHolderOfArrays);
+
+        // display content
+        //console.log(dataHolderOfArrays);
+    });
+
+}
+
+function isData(cell) {
+    if (cell) {
+        return cell.$t;
+    }
+    return " ";
+}
