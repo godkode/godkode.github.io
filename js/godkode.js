63225421e4e4b7ec3f0197407cd7344637b09dc3
Troels H. Jensen <lord_ni@hotmail.com>
2017-08-23 11:03:55 +0200

    Updated the  eventCounter to 0

--- js/godkode.js
+++ js/godkode.js
@@ -4,12 +4,12 @@ var divEL; // GLOBAL: So i can write to it any where
 var spreadsheetID = "1kyftHPSQ6t_YRkTD__kS78QlOPYENnGRRmwx_pGZQpY";
 // Count a hole event for this function
 // jump over 1 event plus this variable with 1
-var eventCounter = 9; // +1 is a whole week / add +1 to skip a week (from tuesday to tuesday)
+var eventCounter = 0; // +1 is a whole week / add +1 to skip a week (from tuesday to tuesday)
 
 function eventProgess() {
-	// event start on 3 and every event takes out 8 spaces
-	//console.log(3 + 8 * eventCounter);
-	return (3 + 8 * eventCounter);
+    // event start on 3 and every event takes out 8 spaces
+    //console.log(3 + 8 * eventCounter);
+    return (3 + 8 * eventCounter);
 }
 
 /**
@@ -18,7 +18,7 @@ function eventProgess() {
 function checkFileAPI() {
     if (window.File && window.FileReader && window.FileList && window.Blob) {
         reader = new FileReader();
-        return true; 
+        return true;
     } else {
         alert('The File APIs are not fully supported by your browser. Fallback required.');
         return false;
@@ -38,82 +38,82 @@ var countCell = 0;
 // Main HTML builder
 // Parameter single Array with max length of 7
 function htmlReturn(txt) {
-  	var valueReturn = "";
+    var valueReturn = "";
 
-	// This is the Div of the coloum.. :)
-	valueReturn += "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-4 borderMeTop'>";
+    // This is the Div of the coloum.. :)
+    valueReturn += "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-4 borderMeTop'>";
 
-	var title = "<H4>" + txt   [2] + "</H4>";
+    var title = "<H4>" + txt[2] + "</H4>";
 
-	var status = "<ul><li><b>Status: </b>" + txt[1] + "</li>"; 
-	var time = "<li><b>Date: </b>" + txt[0] + "</li>";
-	var room = "<li><b>Room: </b>" + txt[6] + "</li>";
-	var who = "<li><b>Who: </b> " + txt[7] + "</li>"; 
-		
-	var invited = "<li><b>Invited: </b> " + txt[5] + "</li>";
+    var status = "<ul><li><b>Status: </b>" + txt[1] + "</li>";
+    var time = "<li><b>Date: </b>" + txt[0] + "</li>";
+    var room = "<li><b>Room: </b>" + txt[6] + "</li>";
+    var who = "<li><b>Who: </b> " + txt[7] + "</li>";
 
-	var description = "<li><b>Description: </b></li>" + txt[3];
+    var invited = "<li><b>Invited: </b> " + txt[5] + "</li>";
 
-	valueReturn += 	title + "" +
-					status + "" +
-				   	time + "" +  
-				   	room + "" +  
-				   	who + "" +  
-				   	invited + "" +  
-				   	description;
-	valueReturn += "</ul>";
-	valueReturn += "</div>";
+    var description = "<li><b>Description: </b></li>" + txt[3];
 
-    if(txt[2].length > 0) {
+    valueReturn += title + "" +
+        status + "" +
+        time + "" +
+        room + "" +
+        who + "" +
+        invited + "" +
+        description;
+    valueReturn += "</ul>";
+    valueReturn += "</div>";
+
+    if (txt[2].length > 0) {
         valueReturn = valueReturn;
         countCell++;
-    } else if(txt[2] <= 0) {
+    } else if (txt[2] <= 0) {
         valueReturn = "";
     }
 
-  	// Html build DONE 
-  	
-  	return valueReturn;
+    // Html build DONE 
+
+    return valueReturn;
 }
 
 // data put in eventDataArray
 // BUILD: HTML from here
 // All data lost after getJSON
 function getValue(event) {
-	if (countCell % 3 == 0) {
-  		divEL.innerHTML += "<div class='row'>";
-  		//console.log("Start row");
-  	}
+    if (countCell % 3 == 0) {
+        divEL.innerHTML += "<div class='row'>";
+        //console.log("Start row");
+    }
 
-	divEL.innerHTML += htmlReturn(event);
+    divEL.innerHTML += htmlReturn(event);
 
-	if (countCell > 2 && countCell % 3 == 2) {
-		// End Row
-		divEL.innerHTML += "</div>";
-		//console.log("End row");
-	} else if (countCell == 2) {
-		// End Row
-		divEL.innerHTML += "</div>";
-		//console.log("End row");
-	}
+    if (countCell > 2 && countCell % 3 == 2) {
+        // End Row
+        divEL.innerHTML += "</div>";
+        //console.log("End row");
+    } else if (countCell == 2) {
+        // End Row
+        divEL.innerHTML += "</div>";
+        //console.log("End row");
+    }
 }
 
 // New reader from 
 // SOURCE: https://ctrlq.org/code/20004-google-spreadsheets-json
 function readSheet() {
-	
+
     // ID of the Google Spreadsheet
     // var spreadsheetID = "1kyftHPSQ6t_YRkTD__kS78QlOPYENnGRRmwx_pGZQpY";
- 
+
     // Make sure it is public or set to Anyone with link can view 
     var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
-    
+
     //console.log(url3);
-	var dataHolderOfArrays = [];
+    var dataHolderOfArrays = [];
 
     $.getJSON(url, function(data) {
- 
-        if(typeof(data) == null) {
+
+        if (typeof(data) == null) {
             console.log("Data == null");
         }
         var entry = data.feed.entry;
@@ -121,16 +121,16 @@ function readSheet() {
         //console.log(entry);
 
         for (var i = eventProgess(); i < entry.length; i++) {
-            
+
             var dataHolderArray = [];
             var counterArrat = 0;
-            
+
             var date = entry[i].gsx$day.$t + " " + entry[i].gsx$info.$t;
             dataHolderArray[counterArrat] = date;
             counterArrat++;
 
             i++;
-			// i = 4
+            // i = 4
 
             // status
             dataHolderArray[counterArrat] = isData(entry[i].gsx$info);
@@ -139,7 +139,7 @@ function readSheet() {
             // title
             dataHolderArray[counterArrat] = isData(entry[i].gsx$title);
             counterArrat++;
-            
+
             // description;
             dataHolderArray[counterArrat] = isData(entry[i].gsx$description);
             counterArrat++;
@@ -160,7 +160,7 @@ function readSheet() {
 
             //var invite = entry[i].gsx$_cpzh4.$t;
             i++;
-			// i = 7
+            // i = 7
 
             // room
             dataHolderArray[counterArrat] = isData(entry[i].gsx$info);
@@ -171,7 +171,7 @@ function readSheet() {
 
             // company/who
             dataHolderArray[counterArrat] = isData(entry[i].gsx$info);
-                      
+
             i += 2;
             // i = 10
             //console.log("dHA: " + dataHolderArray)
